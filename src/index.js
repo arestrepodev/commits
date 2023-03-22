858a5929a1afc7ed9174e0ae221adc0a63c54f07
Arnold Restrepo <restrepo_arnold@outlook.com>
2023-03-21 19:34:26 -0500

    undefined

---
+++ src/index.js
@@ -0,0 +1,88 @@
+import { confirm, intro, isCancel, multiselect, outro, select, text } from '@clack/prompts'
+import { getChangedFiles, getStagedFiles, gitAdd, gitCommit } from './git.js'
+
+import { COMMIT_TYPES } from './commit-types.js'
+import colors from 'picocolors'
+import { trytm } from '@bdsqqq/try'
+
+intro(
+  colors.inverse(`Asistente para la creación de commits por ${colors.blue('@arestrepodev')}`)
+)
+
+const [changedFiles, errorChangedFiles] = await trytm(getChangedFiles())
+const [stagedFiles, errorStagedFiles] = await trytm(getStagedFiles())
+
+if (errorChangedFiles ?? errorStagedFiles) {
+  outro(colors.red('Opps! Al parecer no estas en un repositorio de GIT!'))
+  process.exit(1)
+}
+
+if (stagedFiles.length === 0 && changedFiles.length > 0) {
+  const files = await multiselect({
+    message: colors.cyan('Selecciona los archivos que deseas agregar:'),
+    options: changedFiles.map(file => ({
+      value: file,
+      label: file
+    }))
+  })
+
+  if (isCancel(files)) {
+    outro(colors.yellow('No hay archivos para commitear'))
+    process.exit(0)
+  }
+
+  await gitAdd({ files })
+}
+
+console.log(changedFiles, stagedFiles)
+
+const commitType = await select({
+  message: colors.cyan('Selecciona el tipo de commit'),
+  options: Object.entries(COMMIT_TYPES).map(([key, value]) => ({
+    value: key,
+    label: `${value.emoji} ${key.padEnd(10, ' ')} · ${value.description}`
+  }))
+})
+
+const commitMsg = await text({
+  message: colors.cyan('Introduce el mensaje del commit'),
+  validate: (value) => {
+    if (value.length === 0) {
+      return colors.red('El mensaje no puede estar vacío')
+    }
+    if (value.length > 50) {
+      return colors.red('El mensaje no puede tener más de 100 caracteres')
+    }
+  }
+})
+
+const { emoji, release } = COMMIT_TYPES[commitType]
+
+let breakingChange = false
+
+if (release) {
+  breakingChange = await confirm({
+    initialValue: false,
+    message: `${colors.cyan('¿Este commit tiene cambios que rompen la compatibilidad anterior:')} 
+    ${colors.yellow('Si es así deberías crear un commit con el tipo "BREAKING-CHANGE" y al hacer release se publicará una versión major')}`
+  })
+}
+
+let commit = `${emoji} ${commitType}: ${commitMsg}`
+commit = breakingChange ? `${commit} [Breaking change]` : commit
+
+const shouldBeContinue = await confirm({
+  initialValue: true,
+  message: `${colors.cyan('¿Quieres crear el commit con el siguiente mensaje:')} ${colors.green(colors.bold(commit))}?
+    ${colors.cyan('¿Confirmas?')}
+  `
+})
+
+if (!shouldBeContinue) {
+  outro(colors.yellow('No se ha creado el commit'))
+  process.exit(0)
+}
+
+await gitCommit(commit)
+
+outro(colors.green('✓ Commit creado con éxito!! Gracias por usar el asistente'))
